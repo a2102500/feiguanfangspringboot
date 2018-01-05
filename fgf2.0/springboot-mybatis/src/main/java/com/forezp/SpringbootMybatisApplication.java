60d1b1f8d3fffe4f2f6965070bada903a1b9e866
jojo <a2102500@gmail.com>
2018-01-05 15:11:06 +0800

    2.0
    sb+mybatis

---
+++ fgf2.0/springboot-mybatis/src/main/java/com/forezp/SpringbootMybatisApplication.java
@@ -0,0 +1,14 @@
+package com.forezp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class SpringbootMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMybatisApplication.class, args);
+	}
+}
